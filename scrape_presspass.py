36fd460c23b7ad15558f4a98817b0a111399e1d4
Gaurav Sood <gsood07@gmail.com>
2015-04-24 10:02:21 -0400

    Added scraping scripts

---
+++ scrape_presspass.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+import sys
+import os
+from scraper import SimpleScraper
+
+BASE_URL = 'http://www.presspass.me/journalist'
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: %s <input file>" % (__file__))
+        sys.exit()
+
+    if not os.path.exists('./presspass'):
+        os.mkdir('./presspass')
+
+    f = open(sys.argv[1])
+    reader = csv.DictReader(f)
+    scraper = SimpleScraper()
+    i = 0
+    count = 0
+    for r in reader:
+        count += 1
+        name = r['twitter.username']
+        print(count, "==>", name)
+        html = scraper.get(BASE_URL + '/%s' % name)
+        if html:
+            with open('presspass/%s.html' % name, "wb") as f:
+                f.write(str.encode(html))
+            i += 1
+    print("Found: %d" % i)
+    f.close()
